a4eec2909af940c78de08392ecfc5acc5ec27e1b
Freddy Orozco <freddycamilo95@gmail.com>
2019-02-13 19:47:47 -0500

    Visual Desing it's almost finished

--- src/components/AuxBar/AuxBar.js
+++ src/components/AuxBar/AuxBar.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 import './AuxBar.css';
+import logo from '../../assets/img/CB_Logo.svg';
+import drop_icon from '../../assets/img/arrow_icon.svg';
 
 class AuxBar extends Component {
     render() {
         return (
             <div className="AuxBar">
-                <img src="#" alt="MainLogo" />
-                <div className="Navigation">
-                    <div>
-                        <ul>
+                <div className="AuxBar__LimiterContainer">
+                    <div className="AuxBar__LogoContainer"> <img src={logo} alt="MainLogo" /></div>
+                    <div className="AuxBar__Container">
+                        <ul className="AuxBar__ListContainer">
                             <li> <p>Features</p> </li>
                             <li> <p>Plans</p> </li>
-                            <li> <p>Organizations <img src="#" /></p> </li>
+                            <li> <p>Organizations </p> <div className="AuxBar__ImgContainer"><img src={drop_icon} /></div></li>
                             <li> <p>Browse Courses</p> </li>
                             <li> <p>Support</p> </li>
-                        </ul></div>
-                    <div className="SecondaryNav">
-                        <button>Sing in</button>
-                        <button>7 day trial</button>
+                        </ul>
                     </div>
-
+                    <div className="AuxBar__SecondaryNav">
+                        <button className="Button__Default">Sing in</button>
+                        <button className="Button__Green">7 day trial</button>
+                    </div>
                 </div>
             </div>
         );
